97538d4291f9ca3b41caaaf56dc3cf640dea375c
adwaithks <adwaith2507@gmail.com>
2022-03-29 14:13:12 +0530

    admin dashboard completed

--- src/pages/AdminDashboard/AdminDashboard.jsx
+++ src/pages/AdminDashboard/AdminDashboard.jsx
@@ -8,15 +8,25 @@ import Modal from 'react-modal';
 
 
 function AdminDashboard() {
-
+  const [generating, setGenerating] = React.useState(false);
+  const [publish, setPublish] = React.useState(false);
   const [currentView, setCurrentView] = React.useState('pending');
-  const [subject, setSubject] = React.useState('');
-  const [partADiff, setPartADiff] = React.useState('');
-  const [partBDiff, setPartBDiff] = React.useState('');
+  const [subject, setSubject] = React.useState('English');
+  const [partADiff, setPartADiff] = React.useState('easy');
+  const [partBDiff, setPartBDiff] = React.useState('easy');
   const [modalIsOpen, setIsOpen] = React.useState(false);
+  const [previewIsOpen, setPreviewIsOpen] = React.useState(false);
 
   function openModal() {
     setIsOpen(true);
+  }
+
+  function openPreviewModal() {
+    setPreviewIsOpen(true);
+  }
+
+  function closePreviewModal() {
+    setPreviewIsOpen(false);
   }
 
   function closeModal() {
@@ -30,26 +40,101 @@ function AdminDashboard() {
   return (
     <div className='admin-dashboard'>
       <Modal
+        className='preview-modal'
+        isOpen={previewIsOpen}
+        onRequestClose={closePreviewModal}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.9)'
+          }
+        }}
+      >
+        <div className='pdf-previewer'>
+          <h1>Nothing to Preview</h1>
+        </div>
+        <div className='publish-btn-container'>
+          {
+            publish ? (<button style={{ opacity: 0.7 }}>Publishing...</button>) : (<button onClick={() => {
+              setPublish(true);
+              setTimeout(() => { closePreviewModal() }, 3000);
+            }}>Publish</button>)
+          }
+        </div>
+      </Modal>
+
+
+      <Modal
         className="admin-qpgen-modal"
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={{
           overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.6)'
+            backgroundColor: 'rgba(0, 0, 0, 0.9)'
           }
         }}
       >
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={subject}
-          label="Age"
-          onChange={handleChange}
-        >
-          <MenuItem value={'English'}>English</MenuItem>
-          <MenuItem value={'Physics'}>Physics</MenuItem>
-          <MenuItem value={'Chemistry'}>Chemistry</MenuItem>
-        </Select>
+        <div className='sub-select-container'>
+          <h1>Select Subject</h1>
+          <Select
+            labelId="demo-simple-select-label"
+            className='sub-select'
+            value={subject}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={'English'}>English</MenuItem>
+            <MenuItem value={'Physics'}>Physics</MenuItem>
+            <MenuItem value={'Chemistry'}>Chemistry</MenuItem>
+          </Select>
+        </div>
+        <div className='part-a-diff-container'>
+          <div className='part-a-head'>
+            <h1>Part A Difficulty</h1>
+          </div>
+          <div className='part-a-diff'>
+            {
+              partADiff == 'easy' ? (
+                <h1 className='part-a-active'>Easy</h1>
+              ) : (<h1 onClick={() => { setPartADiff('easy') }}>Easy</h1>)
+            }
+            {
+              partADiff == 'medium' ? (<h1 className='part-a-active'>Medium</h1>) : (<h1 onClick={() => { setPartADiff('medium') }}>Medium</h1>)
+            }
+            {
+              partADiff == 'hard' ? (<h1 className='part-a-active'>Hard</h1>) : (<h1 onClick={() => { setPartADiff('hard') }}>Hard</h1>)
+            }
+          </div>
+        </div>
+        <div className='part-b-diff-container'>
+          <div className='part-b-head'>
+            <h1>Part B Difficulty</h1>
+          </div>
+          <div className='part-b-diff'>
+            {
+              partBDiff == 'easy' ? (
+                <h1 className='part-b-active'>Easy</h1>
+              ) : (<h1 onClick={() => { setPartBDiff('easy') }}>Easy</h1>)
+            }
+            {
+              partBDiff == 'medium' ? (<h1 className='part-b-active'>Medium</h1>) : (<h1 onClick={() => { setPartBDiff('medium') }}>Medium</h1>)
+            }
+            {
+              partBDiff == 'hard' ? (<h1 className='part-b-active'>Hard</h1>) : (<h1 onClick={() => { setPartBDiff('hard') }}>Hard</h1>)
+            }
+          </div>
+        </div>
+        <div className='qp-publish-container'>
+          {
+            generating ? (<button style={{ opacity: 0.7 }}>Generating...Please Wait</button>) : (<button onClick={() => {
+              setGenerating(true);
+              setTimeout(() => {
+                closeModal();
+                openPreviewModal();
+              }, 3000);
+            }}>Generate Question Paper</button>)
+          }
+
+        </div>
       </Modal>
       <div className='qp-gen-container' onClick={() => { openModal() }}>
         <h1><AiOutlineFileAdd className='gen-qp-icon' />Generate Question Paper</h1>
---
+++ src/pages/TeacherDashboard/TeacherDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TeacherDashboard() {
+  return (
+    <div className='teacher-dashboard'>
+
+    </div>
+  )
+}
+
+export default TeacherDashboard;
